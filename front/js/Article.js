2a18d74e07e5316dd9ec328b0efafaaeb0c225d5
Gui <gsetienne@free.fr>
2021-11-01 22:17:02 +0200

    JS page d'accueil OK

--- front/js/Article.js
+++ front/js/Article.js
@@ -13,8 +13,18 @@ class Article {
         container.setAttribute("href","./product.html?id=" + this.id);
         const article=document.createElement("article");
         container.appendChild(article);
-        const image=document.createElement("img")
-        container.appendChild(image);
+        const img=document.createElement("img")
+        article.appendChild(img);
+        img.setAttribute("src", this.imageUrl);
+        img.setAttribute("alt", this.altTxt);
+        const titreH3=document.createElement("h3");
+        article.appendChild(titreH3);
+        titreH3.setAttribute("class", "productName");
+        titreH3.innerHTML=this.name;
+        const descript=document.createElement("p");
+        article.appendChild(descript);
+        descript.setAttribute("class", "productDescription");
+        descript.innerHTML=this.description;
         // image, id, srv etc... faire tous les champs 
         
         return container;
